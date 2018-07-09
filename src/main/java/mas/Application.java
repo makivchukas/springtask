909201d56b20c85bb2391dfb73912805a52eaed4
Anton Makivchuk <makivchukas@gmail.com>
2018-07-09 10:30:32 +0000

    Good working put

--- src/main/java/mas/Application.java
+++ src/main/java/mas/Application.java
@@ -5,80 +5,38 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
 import java.util.Scanner;
-import java.io.IOException;
+import javax.imageio.ImageIO;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
-
-import javax.imageio.ImageIO;
 
 public class Application {
     public static void main(String[] args)throws Exception{
-        System.out.println("Start");
+        System.out.println("Start");        
         Server server = new Server(8080);
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setDirectoriesListed(true);
-        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
-        resource_handler.setResourceBase(".");
-        
-        
-       ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-       
-       context.setContextPath("/");
+
+       ServletContextHandler context = new ServletContextHandler(
+                ServletContextHandler.SESSIONS);
+                context.setContextPath("/");
         server.setHandler(context);
 
         context.addServlet(HelloServlet.class, "/");
          context.addServlet(ImageServlet.class, "/image");
-
-        server.start();
-        server.join();
-       try{ getInterfacesAddress();
-       }catch(SocketException e) {
-           e.printStackTrace();
-       }
-
-                System.out.println("Stop");
-
-    } 
-    
-    public static void getInterfacesAddress()throws SocketException{
-                Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
-        for (; n.hasMoreElements();){
-            NetworkInterface e = n.nextElement();
-            System.out.println(e);
-            
-            Enumeration<InetAddress> a = e.getInetAddresses();
-            for (; a.hasMoreElements();) {
-                InetAddress addr = a.nextElement();
-                String getHostAddress = addr.getHostAddress();
-                System.out.println(getHostAddress);
-//                if (checkIPv4(getHostAddress)) {
-//                    combo.addItem(addr.getHostAddress() + " # " + e.getDisplayName().toString());
-//                }
-            }
-        }
-    }
-
         
-    
+        // Start things up!
+        server.start();
 
+        server.join();        
+    } 
     
     @SuppressWarnings("serial")
     public static class HelloServlet extends HttpServlet
@@ -89,9 +47,11 @@ public class Application {
                                                             IOException
         {
             File indexHtml = new File("index.html");
+            InputStream stream=Application.class.getClassLoader().getResourceAsStream("webapp/index.html");
             response.setContentType("text/html");
             response.setStatus(HttpServletResponse.SC_OK);
-            Scanner reader = new Scanner(new FileReader(new File("index.html")));
+//            Scanner reader = new Scanner(new FileReader(new File("index.html")));
+            Scanner reader = new Scanner(stream);
             while(reader.hasNextLine()){
                 response.getWriter().println(reader.nextLine());
             }
